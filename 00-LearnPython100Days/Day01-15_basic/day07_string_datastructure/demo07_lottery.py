9fb74ef2dd682b6758a9cc7753c6d62b8607ade3
Weipu Shan <weipu.shan@gmail.com>
2020-08-11 01:16:21 +0200

    string and data structure

--- 00-LearnPython100Days/Day01-15_basic/day07_string_datastructure/demo07_lottery.py
+++ 00-LearnPython100Days/Day01-15_basic/day07_string_datastructure/demo07_lottery.py
@@ -1,5 +1,5 @@
 """
-双色球随机选号程序
+双色球随机选号程序, 6 + 1, 从1-33中选取六个，从1-16中选取一个
 
 Version: 0.1
 Author: 骆昊
@@ -24,16 +24,23 @@ def random_select():
     """
     随机选择一组号码
     """
-    red_balls = [x for x in range(1, 34)]
+    # 从 1- 33 中选择六个随机数，不放回，即在选取之后应该删除
+    red_balls = [x for x in range(1, 34)]       # number from 1 - 33
     selected_balls = []
+
     for _ in range(6):
-        index = randrange(len(red_balls))
+        index = randrange(len(red_balls))       # randrange() 从递增基数集合中返回一个随机值
         selected_balls.append(red_balls[index])
-        del red_balls[index]
+        del red_balls[index]                    # 删除被选中的红色球
+
     # 上面的for循环也可以写成下面这行代码
-    # sample函数是random模块下的函数
+    # sample函数是random模块下的函数, 实现从列表中选择不重复的n个元素。
     # selected_balls = sample(red_balls, 6)
+
+    # 把选择的6个数进行排序
     selected_balls.sort()
+
+    # 重新从1-16中选择一个随机数
     selected_balls.append(randint(1, 16))
     return selected_balls
 
